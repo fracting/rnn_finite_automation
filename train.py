3b457809549b3d1f9e4e552c0abaf86e8289642f
Qian Hong <fracting@gmail.com>
2018-04-29 23:00:45 +1000

    Increase training dataset size to 16384, increase total epoch to 10000.

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ BATCH_SIZE = 256
 
 print_per_epoch = 100
 print_per_batch = 100
-total_epoch = 6000
+total_epoch = 10000
 
 learning_rate = 0.0025 * BATCH_SIZE
 
@@ -26,7 +26,7 @@ model = DFA(EMBEDDING_DIM, HIDDEN_DIM, len(char_to_ix), len(category_to_ix), NUM
 loss_function = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
-truncate_size = 8192
+truncate_size = 16384
 all_training_data = load_training_data("dataset/10div7.txt")
 print("all_training_data size: %d" % len(all_training_data))
 training_data = all_training_data[0:truncate_size]
