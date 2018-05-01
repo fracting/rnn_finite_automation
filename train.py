4193479645e1e334f44c6aae915e714f6510115d
Qian Hong <fracting@gmail.com>
2018-05-02 05:04:32 +1000

    Decrease hidden dim to 12.

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from model import DFA
 from data import char_to_ix, category_to_ix, seqs_to_tensor, categories_to_tensor, load_dataset
 
 EMBEDDING_DIM = 15
-HIDDEN_DIM = 14
+HIDDEN_DIM = 12
 NUM_LAYERS = 1
 BATCH_SIZE = 128
 DROPOUT = 0.0 # dropout does not apply on output layer, so no effect to single layer network
