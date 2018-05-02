787944982808d0253c7295aaf19df19b3233b9a4
Qian Hong <fracting@gmail.com>
2018-05-02 15:43:36 +1000

    Train on 10div11.balance.txt.

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ cont_train_size = 8571
 rand_train_size = 16384
 cont_valid_size = 8571
 rand_valid_size = 16384
-dataset_path = "10div7.balance.txt"
+dataset_path = "10div11.balance.txt"
 dataset, vocab_size, category_size = load_dataset("dataset/"+dataset_path, cont_train_size, rand_train_size, cont_valid_size, rand_valid_size)
 EMBEDDING_DIM = vocab_size * 2
 
