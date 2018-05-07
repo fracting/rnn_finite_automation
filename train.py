1250e612d01e1d8d3493991371ff28c4c8a87926
Qian Hong <fracting@gmail.com>
2018-05-07 18:29:11 +1000

    Also works.

--- train.py
+++ train.py
@@ -121,7 +121,7 @@ def validation(data_name, dump_hidden, counter, training_accuracy):
         training_cache.sort(reverse = True, key = lambda x: x[1])
         print(training_cache[:3])
         #if counter % update_per_counter == 0:
-        if training_accuracy > 0.99:
+        if training_accuracy > 0.95:
             print("update training set")
             dataset["dyna_train"] = dataset["dyna_train"] + list(list(zip(*training_cache[:512]))[0])
         sys.stdout.flush()
