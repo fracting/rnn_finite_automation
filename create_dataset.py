1a6398253ec99f25220a907e0aa3481c6a8d4463
Qian Hong <fracting@gmail.com>
2018-05-07 10:05:51 +1000

    Create larger dataset.

--- create_dataset.py
+++ create_dataset.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def classify(num, divider, class_type):
     if class_type == "imbalance":
         res = int(num % divider == 0)
@@ -15,15 +17,19 @@ def classify(num, divider, class_type):
 
     return res
 
-def create_data(start, end, base, divider, class_type):
+def create_data(start, mid, count, max, base, divider, class_type):
     if base != 10:
         raise NotImplementedError("base other than 10 not implemented yet")
 
     lines = []
-    for i in range(start, end):
-        res = classify(i, divider, class_type)
+    for i in range(start, count):
+        if i < mid:
+            x = i
+        else:
+            x = randint(mid, max)
+        res = classify(x, divider, class_type)
         if res != "Other":
-            lines.append(str(i) + "," + str(res))
+            lines.append(str(x) + "," + str(res))
 
     path = "dataset/" + str(base) + "div" + str(divider) + "." + class_type + ".txt"
     file = open(path, "w+")
@@ -31,14 +37,4 @@ def create_data(start, end, base, divider, class_type):
     file.write(output)
     file.close
 
-create_data(0, 100000, 10, 7, "imbalance")
-create_data(10000, 100000, 10, 7, "balance")
-create_data(10000, 100000, 10, 7, "multiclass")
-
-create_data(10000, 100000, 10, 11, "imbalance")
-create_data(10000, 100000, 10, 11, "balance")
-create_data(10000, 100000, 10, 11, "multiclass")
-
-create_data(10000, 100000, 10, 5, "multiclass")
-create_data(10000, 100000, 10, 8, "multiclass")
-create_data(0, 100000, 10, 16, "multiclass")
+create_data(0, 100000, 200000, 1000000000, 10, 16, "multiclass")
