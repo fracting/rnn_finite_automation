1a5a9ddc0ffaad762512f8e1250544dd86aedcfa
Qian Hong <fracting@gmail.com>
2018-05-07 21:23:46 +1000

    Best hyperparameters for 10div7.imbalance
    train ['cont_train', 'dyna_train'] size 15872 for 500 epoch
    
    time spent in 1 epoch 0:00:00.608623
    ['cont_train', 'dyna_train'] training epoch 67 loss 0.005769 accuracy 0.999935
    
    Evaluating cont_valid: loss 0.001907 accuracy 1.000000
    [((['3', '4', '5', '0', '9', '1', '4', '8', '8'], '1'), 0.42891377210617065), ((['2', '8', '7', '0', '5', '3', '1', '3'], '1'), 0.29119423031806946), ((['9', '7', '8', '4', '6', '6', '1', '1', '1'], '1'), 0.26953399181365967)]
    update training set
    saving checkpoint

--- train.py
+++ train.py
@@ -121,9 +121,9 @@ def validation(data_name, dump_hidden, counter, training_accuracy):
         training_cache.sort(reverse = True, key = lambda x: x[1])
         print(training_cache[:3])
         #if counter % update_per_counter == 0:
-        if training_accuracy > 0.93:
+        if training_accuracy > 0.90:
             print("update training set")
-            dataset["dyna_train"] = dataset["dyna_train"] + list(list(zip(*training_cache[:128]))[0])
+            dataset["dyna_train"] = dataset["dyna_train"] + list(list(zip(*training_cache[:256]))[0])
         sys.stdout.flush()
 
     return average_loss
